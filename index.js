1e0f4062a7492f135ce7b19338d6f1431de89018
hsantos4 <hector.santos597@gmail.com>
2023-03-04 09:25:28 -0500

    Added the hamburger menu, the svg code, to the nav html tag, we need this in there for mobile. Fixed the navigation for larger screens, the width for .nav-container was set to 20% so all the list items were on each other. For the the class nav-group-lg-screens, we dont want them showing on the small screens. In the media query  nav-group-lg-screens made visible and made the hamburger menu not.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let greeting = "hello, world";
-let results = `This is a variable I created ${greeting}`;
+const mobileMenu = document.querySelector('.mobileMenu');
 
-console.log(results)
-
-
-    
+// When the hamburger menu is click
+mobileMenu.addEventListener('click', () => {
+// We want to create a div element
+// within that div we want to create a ul with li items and anchor tags
+// 
+})
     
 
 
